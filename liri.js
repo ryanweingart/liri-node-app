82a11342fbc0c085d912106976be2b7eb4843fb5
Ryan Weingart <rmweingart5@gmail.com>
2019-08-09 18:48:39 -0400

    added descriptions to liri.js

--- liri.js
+++ liri.js
@@ -17,7 +17,7 @@ var spotify = new Spotify({
   secret: spotifySecret
 });
  
-
+//Function that will query song using the Spotify module
 function spotifySearch(songName) {
 
     spotify.search({ type: 'track', query: songName }, function(err, data) {
@@ -26,9 +26,9 @@ function spotifySearch(songName) {
         }
         
         var songs = data.tracks.items;
+
         for (var i = 0; i < songs.length; i++) {
-            log(i);
-            log("artist(s): " + songs[i].artists.map(artistName));
+            log("artist(s): " + songs[i].artists[0].name);
             log("song name: " + songs[i].name);
             log("preview song: " + songs[i].preview_url);
             log("album: " + songs[i].album.name);
@@ -37,6 +37,7 @@ function spotifySearch(songName) {
     });
 }
 
+//Function that will query movies using the OMDB api
 function movieSearch(movie) {
 
     var apiURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movie;
@@ -45,35 +46,37 @@ function movieSearch(movie) {
         var movieData = response.data;
 
         log("Title: " + movieData.Title);
+        log("Actors/Actresses: " + movieData.Actors);
         log("Release Year: " + movieData.Year);
-        log("IMDB Rating: " + movideData.imdbRating);
+        log("IMDB Rating: " + movieData.imdbRating);
         log("Rotten Tomatoes Rating: " + movieData.tomatoRating);
         log("Country of Production: " + movieData.Country);
         log("Language: " + movieData.Language);
         log("Plot: " + movieData.Plot);
-        log("Actors/Actresses: " + movieData.Actors);
     })
 }
 
+//Function that will query concerts using the Bands in Town api
 function concertSearch() {
-    var artistName = process.argv.slice(3).join(" ");
+    var bandName = process.argv.slice(3).join(" ");
 
-    var bandsURL = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
+    var bandsURL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
 
     axios.get(bandsURL).then(function(response) {
 
         for (var i = 0; i < response.data.length; i++) {
             var bandData = response.data[i];  
             
-            log("================================================")
             log("Artist: " + bandData.lineup);
             log("Venue: " + bandData.venue.name);
             log("Location: " + bandData.venue.city);
             log("Date: " + moment(bandData.datetime).format("MM/DD/YYYY"));
+            log("=========================================================");
         }
     })
 }
 
+//Function that will use the random.txt file to display data
 function doWhatItSays () {
 
     fs.readFile("random.txt", "utf8", function(error, data) {
@@ -91,6 +94,8 @@ function doWhatItSays () {
         }
     })
 }
+
+//Switch-case function that holds user possibilities for results
 function userActions(caseData, functionData) {
     switch(caseData) {
         case "spotify-this-song":
@@ -110,6 +115,7 @@ function userActions(caseData, functionData) {
     }
 }
 
+//Function that takes in user inputs
 function userInput(argOne, argTwo) {
     userActions(argOne, argTwo);
 };
